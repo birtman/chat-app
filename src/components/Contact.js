86ca22d1b0e9efe9179cdf142a1b136ed2b57e18
Birte Spreuer <mail@birtespreuer.com>
2020-02-19 11:14:57 +0100

    first commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Contact.css";
+import { render } from "@testing-library/react";
 
-function Contact(props) {
-  return (
-    <div className="Contact">
-      <img className="avatar" src={props.nameavatar} alt={props.name} />
-      <h4 className="name">{props.name}</h4>
-      <p className="status">
-        <span
-          className={
-            props.status === "online" ? "status-online" : "status-offline"
-          }
-        ></span>
-        {props.status}
-      </p>
-    </div>
-  );
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: false
+    };
+  }
+
+  render() {
+    return (
+      <div className="Contact">
+        <img
+          className="avatar"
+          src={this.props.nameavatar}
+          alt={this.props.name}
+        />
+        <h4 className="name">{this.props.name}</h4>
+        <p className="status">
+          <span
+            className={this.state.online ? "status-offline" : "status-online"}
+            onClick={event => {
+              const nowOffline = !this.state.online;
+              this.setState({ online: nowOffline });
+            }}
+          ></span>
+          {this.props.status}
+        </p>
+      </div>
+    );
+  }
 }
 
 export default Contact;
